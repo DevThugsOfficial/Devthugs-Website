a183edebe54b98ed4de93b8ddd147018c1dfa15c
EP4CXS <epacxzs@gmail.com>
2026-08-02 22:24:58 +0800

    Use Node 22.x, latest vercel-php, and set storage path in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__));
+
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || file_exists('/tmp')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -17,3 +23,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
