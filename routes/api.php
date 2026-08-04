71439d87e0294ddf3e233213c932c5ab95f3aa4f
EP4CXS <epacxzs@gmail.com>
2026-08-04 23:31:35 +0800

    Remove buildCommand from vercel.json and clean up unused user route in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\ChatbotController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// Chatbot routes
 Route::prefix('chatbot')->group(function () {
     Route::post('/message', [ChatbotController::class, 'sendMessage']);
     Route::get('/session/{token}/messages', [ChatbotController::class, 'getMessages']);
